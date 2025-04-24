c1698cd912e647c00a33704b2b86e842aed7e797
Wynter Japp <knotkneeded127@gmail.com>
2025-04-24 12:58:39 -0500

    uncommented transcription num getter

--- StudyBuddyBackend/server.py
+++ StudyBuddyBackend/server.py
@@ -140,9 +140,9 @@ def upload_file():
         #    print(f"Translating transcription directly into {target_language}...")
         #    transcription_text = translate_text(transcription_text, target_language)
         #    print(f"Transcription in {target_language} completed.")
-        #if(session.get("account_num") != None):
-        #   session['folder_num']= storeFolder(mysql, "Test Folder", session.get("account_num"))
-        #  transcription_num = storeTranscription(mysql,"Transcription Name", transcription_text,session.get("account_num"),session.get("folder_num") )
+        if(session.get("account_num") != None):
+           session['folder_num']= storeFolder(mysql, "Test Folder", session.get("account_num"))
+           transcription_num = storeTranscription(mysql,"Transcription Name", transcription_text,session.get("account_num"),session.get("folder_num") )
 
         # Add transcription to results
         results = {
